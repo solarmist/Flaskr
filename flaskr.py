c63ac9a5eb19678390dfc8081f38759e460bbada
Joshua Olson <joshua.olson@gmail.com>
2011-12-17 15:56:25 -0800

    Moved imports to flaskr

--- flaskr.py
+++ flaskr.py
@@ -1,7 +1,26 @@
+from __future__ import with_statement
+from contectlib import closing
+# from flask import abort
+# from flask import flash
 from flask import Flask
+# from flask import g
+# from flask import render_template
+# from flask import request
+# from flask import session
+# from flask import redirect
+# from flask import url_for
+import sqlite3
 
 app = Flask(__name__)
 app.config.from_pyfile('setup.py', silent=False)
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
--- setup.py
+++ setup.py
@@ -1,16 +1,4 @@
-# All the imports
-import sqlite2
-from flask import abort
-from flask import flash
-from flask import Flask
-from flask import g
-from flask import render_template
-from flask import request
-from flask import session
-from flask import redirect
-from flask import url_for
-
-# configuratoin
+# configuration
 DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'some_hard_key'
