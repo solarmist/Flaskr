0de734614d6fcf347544b81034c8c0d3722f992f
Joshua Olson <joshua.olson@gmail.com>
2011-12-17 22:28:01 -0800

    Finished test suite

--- tests/flaskr_tests.py
+++ tests/flaskr_tests.py
@@ -24,6 +24,35 @@ class FlaskrTestCase(TestCase):
         rv = self.app.get('/')
         assert 'No entries here so far' in rv.data
 
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+                username=username,
+                password=password
+                ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        rv = self.login('solarmist', 'niko-angel')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('adminx', 'niko-angel')
+        assert 'Invalid username and/or password' in rv.data
+        rv = self.login('solarmist', 'default')
+        assert 'Invalid username and/or password' in rv.data
+
+    def test_messages(self):
+        self.login('solarmist', 'niko-angel')
+        rv = self.app.post('/add', data=dict(
+                title='<Hello>',
+                text='<strong>HTML</strong> allowed here'
+                ), follow_redirects=True)
+        assert 'No entries here so far' not in rv.data
+        assert '&lt;Hello&gt;' in rv.data
+        assert '<strong>HTML</strong> allowed here' in rv.data
+
     @class_teardown
     def destroy_db(self):
         os.close(self.db_fd)
