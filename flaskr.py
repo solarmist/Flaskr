ed6cd272fc876c52f86cc6b76dd146691c9cc42f
Joshua Olson <joshua.olson@gmail.com>
2011-12-17 16:36:34 -0800

    Finished basic app code

--- flaskr.py
+++ flaskr.py
@@ -1,18 +1,73 @@
 from __future__ import with_statement
 from contectlib import closing
-# from flask import abort
-# from flask import flash
+from flask import abort
+from flask import flash
 from flask import Flask
 from flask import g
-# from flask import render_template
-# from flask import request
-# from flask import session
-# from flask import redirect
-# from flask import url_for
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect
+from flask import url_for
 import sqlite3
 
 app = Flask(__name__)
 app.config.from_pyfile('setup.py', silent=False)
+
+
+@app.route('/')
+def show_entries():
+    cur = query_db('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+             [request.form['title'], request.form['text']])
+    g.db.commit()
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if (request.form['username'] != app.config['USERNAME'] or
+           request.form['password'] != app.config['PASSWORD']):
+            error = 'Invalid username and/or password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+def query_db(query, args=(), one=False):
+    cur = g.db.execute(query, args)
+    rv = [dict((cur.description[idx][0], value)
+               for idx, value in enumerate(row)) for row in cur.fetchall()]
+    return (rv[0] if rv else None) if one else rv
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 
 @app.before_request
@@ -24,16 +79,6 @@ def before_request():
 def teardown_request(exception):
     g.db.close()
 
-
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
 
 if __name__ == '__main__':
     app.run()
